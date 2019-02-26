a905d7fb6f5c80e3c2fdc30f74b6aaf57c90acad
Logan Northcote <l.a.northcote@gmail.com>
2019-02-25 21:31:07 -0800

    First commit.

---
+++ board/precache-manifest.2ea24baea18d8bd7edbcf44e00a19507.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2013762bc69d4bfa1edd",
+    "url": "/static/js/main.2013762b.chunk.js"
+  },
+  {
+    "revision": "0b4f05366a0f311e62d4",
+    "url": "/static/js/1.0b4f0536.chunk.js"
+  },
+  {
+    "revision": "d920c82618e04b1603100b456b600b09",
+    "url": "/index.html"
+  }
+];
